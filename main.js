336036957ae4a6a08a1aabed82b5b9071422fd5d
sanarva <60667980+sanarva@users.noreply.github.com>
2020-09-23 10:49:43 +0200

    Changes to optimice code

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 //Función usada para cambiar de color la huella cuando hacemos click en ella. Con el primer for lo que hacemos es quitar la clase activePaw de todos los elementos para luego, en el segundo for, ponérsela a los elementos que le correspondan.
 function changeColorClick(itemChoosen) {
-    let removeColorPaw = document.getElementsByClassName("fa-paw");
-    for (let i = 0; i < removeColorPaw.length; i++) {
-        removeColorPaw[i].classList.remove("activePaw");
-    }
+    cleanColor();
 
     for (let j = 1; j <= itemChoosen; j++) {
         let colorPaw = document.getElementById("positionPaw" + j);
@@ -13,14 +10,17 @@ function changeColorClick(itemChoosen) {
 
 //Para quitar la clase al hacer click fuera de las huellas, usaremos target
 //Aquí estoy añadiendo el evento "click" al id "body", por lo que solo hace caso si clico en el body
-document.getElementById("body").addEventListener("click", cleanColor);
+document.getElementById("body").addEventListener("click", chekClick);
 
-function cleanColor(e) {
-
+function chekClick(e) {
     if (e.target.nodeName != "I") {
-        let removeColorPaw = document.getElementsByClassName("fa-paw");
-        for (let i = 0; i < removeColorPaw.length; i++) {
-            removeColorPaw[i].classList.remove("activePaw");
-        }
+        cleanColor();
+    }
+}
+
+function cleanColor() {
+    let removeColorPaw = document.getElementsByClassName("fa-paw");
+    for (let i = 0; i < removeColorPaw.length; i++) {
+        removeColorPaw[i].classList.remove("activePaw");
     }
 }
